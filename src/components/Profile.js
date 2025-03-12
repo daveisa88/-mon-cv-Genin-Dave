27c3390285bf73061443e7520118a8bdb1fb5f54
daveisa88 <44738826+daveisa88@users.noreply.github.com>
2025-03-12 18:22:49 +0100

    majMise à jour du CV web

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,10 +2,9 @@ import React from "react";
 
 const Profile = () => {
     return (
-        <div className="relative flex flex-col items-center bg-blue-900 text-white p-10 w-[400px] rounded-lg shadow-lg backdrop-blur-md bg-opacity-90 mx-auto mt-14">
-
+        <div className="relative flex flex-col items-center bg-blue-900 text-white p-4 w-[400px] rounded-lg shadow-lg backdrop-blur-md bg-opacity-90 mx-auto mt-10">
             {/* Photo de profil carrée et intégrée */}
-            <div className="w-32 h-32 bg-white rounded-md overflow-hidden mb-4">
+            <div className="w-30 h-32 bg-white rounded-md overflow-hidden mb-4">
                 <img
                     src="/profile.jpg"
                     alt="Dave Genin"
@@ -14,23 +13,20 @@ const Profile = () => {
             </div>
 
             {/* Informations */}
-            <h2 className="text-4xl font-bold">Dave Genin</h2>
-            <h2 className="text-2xl font-bold">T.A.I (Developpement)</h2>
+            <h2 className="text-4xl font-bold mb-0">Dave Genin</h2>
 
-            {/* Lien vers Google Maps */}
-            <p className="mt-8 text-lg">
-                🏠{" "}
-                <a
-                    href="https://www.google.com/maps/search/Gugnecourt"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-green-400 hover:underline font-semibold"
-                >
-                        311 rue des Aulnées, 88600 Gugnecourt
-                </a>
-            </p>
+            {/* T.A.I en vertical */}
+            <div className="text-3xl font-bold flex flex-col items-center leading-tight">
+                <span className="text-1xl">T</span>
+                <span className="text-lg">Technicien</span>
+                <span className="text-1xl">A</span>
+                <span className="text-lg">Assistance</span>
+                <span className="text-1xl">I</span>
+                <span className="text-lg">Informatique</span>
+            </div>
 
-            <p className="mt-4 text-lg font-semibold">🎂 9 août 1976</p>
+            {/* Mention de cybersécurité */}
+            <p className="text-lg font-semibold mt-0"></p>
         </div>
     );
 };
